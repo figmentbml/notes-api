86699de99b23cd67427de26465b3e6742129e408
figmentbml <beth.jaswa@gmail.com>
2015-01-30 16:02:46 -0700

    updated config app

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module NotesApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        resource '*', :headers => :any, :methods => [:get, :patch, :post, :options, :delete]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
